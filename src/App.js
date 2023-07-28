53dc00de22762fd595ca1abfd4b656ca59f0358e
Jack Stork <jksd2d@umsystem.edu>
2023-07-27 20:48:22 -0500

    First changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// Get date to display next to title
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+let shortDate = `${day}/${month + 1}/${year}`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Title">
+        <text className="TitleName">Poem a Day</text>
+        <text className="TitleDate">{shortDate}</text>
+      </div>
+      <hr className="TitleLine" />
+      <div className="PoemEntry">
+        <div className="PoemEntryDetails">
+          <div className="PoemEntryDetailsSubject">
+            <text className="PoemEntryDetailsSubjectSubject">Subject:</text>
+            <text className="PoemEntryDetailsSubjectValue">Death</text>
+          </div>
+          <div className="PoemEntryDetailsForm">
+            <text className="PoemEntryDetailsFormForm">Form:</text>
+            <text className="PoemEntryDetailsFormValue">Ghazal</text>
+          </div>
+        </div>
+        <div className="PoemEntryBox">
+          <textarea type="text" className="PoemEntryBoxBox" />
+          <button className="PoemEntryBoxSubmit" id="PoemEntryBoxSubmit">
+            ✓
+          </button>
+        </div>
+      </div>
+      <hr className="TitleLine" />
+      <div className="Posts"></div>
     </div>
   );
 }
