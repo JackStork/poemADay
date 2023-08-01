82f32361eabb77c1969a3de45324a8183b1d7750
Jack Stork <jksd2d@umsystem.edu>
2023-08-01 10:45:39 -0500

    Google sign-in, initial Firebase and poem saving

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import handleSubmit from "./handles/handlesubmit";
 
 // Get date to display next to title
 const date = new Date();
@@ -8,6 +12,51 @@ let year = date.getFullYear();
 let shortDate = `${day}/${month + 1}/${year}`;
 
 function App() {
+  // Google login test stuff
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState(null);
+  const [userPoem, setUserPoem] = useState("");
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+          console.log("LOGGED IN");
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+    console.log("LOGGED OUT");
+  };
+
+  // For firebase testing
+  const dataRef = useRef();
+
+  const submithandler = (e) => {
+    e.preventDefault();
+    handleSubmit("TEST");
+    dataRef.current.value = "";
+  };
+
   return (
     <div className="App">
       <div className="Title">
@@ -27,10 +76,23 @@ function App() {
           </div>
         </div>
         <div className="PoemEntryBox">
-          <textarea type="text" className="PoemEntryBoxBox" />
-          <button className="PoemEntryBoxSubmit" id="PoemEntryBoxSubmit">
-            ✓
-          </button>
+          <textarea
+            type="text"
+            className="PoemEntryBoxBox"
+            id="PoemEntryBoxBox"
+            ref={dataRef}
+            //onChange will be used for saving field value when refreshed
+          />
+          <div className="PoemEntryBoxBelow">
+            {profile ? (
+              <button onClick={() => logOut()}>Sign Out</button>
+            ) : (
+              <button onClick={() => login()}>Sign in with Google</button>
+            )}
+            <button onClick={submithandler} className="PoemEntryBoxBelowSubmit">
+              ✓
+            </button>
+          </div>
         </div>
       </div>
       <hr className="TitleLine" />
