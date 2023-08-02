9c542e9803c2dd8c853a2bcb414a0580021f2f01
Jack Stork <jksd2d@umsystem.edu>
2023-08-02 18:02:17 -0500

    Removed unnecessary data field

--- src/handles/handlesubmit.js
+++ src/handles/handlesubmit.js
@@ -4,10 +4,6 @@ import { firestore } from "../firebase_setup/firebase";
 async function handleSubmit(userData, poemData, dateData) {
   const ref = collection(firestore, dateData);
 
-  const data = {
-    poem: poemData,
-  };
-
   await setDoc(doc(firestore, dateData, userData), {
     poem: poemData,
     likes: 0,
